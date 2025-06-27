8f8d6a996c8ab3cc2bd4fcd60ba36b59649a6184
Brendan Viscomi <brendanv@users.noreply.github.com>
2025-06-26 22:53:20 -0400

    Basic auth plumbing using auth0

---
+++ src/auth.ts
@@ -0,0 +1,17 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import Auth0 from '@auth/sveltekit/providers/auth0';
+
+export const { handle, signIn } = SvelteKitAuth((event) => {
+	const authOptions = {
+		providers: [
+			Auth0({
+				issuer: event.platform.env.AUTH_AUTH0_DOMAIN,
+				clientId: event.platform.env.AUTH_AUTH0_ID,
+				clientSecret: event.platform.env.AUTH_AUTH0_SECRET
+			})
+		],
+		secret: event.platform.env.AUTH_SECRET,
+		trustHost: true
+	};
+	return authOptions;
+});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+	import { signIn } from '@auth/sveltekit/client';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div>
+	<button on:click={signIn('auth0')}>Sign In</button>
+</div>
